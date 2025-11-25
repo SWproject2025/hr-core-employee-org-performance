5f20594068fbd64d1b8ac9e5f94291bf6fc20bed
Tahhoon22 <m.tahoon20@gmail.com>
2025-11-25 14:03:30 +0200

    Delete backend directory

--- backend/src/users/index.ts
+++
@@ -1,6 +0,0 @@
-export { UsersModule } from './users.module';
-export { UsersService } from './users.service';
-export { UsersController } from './users.controller';
-export { User, UserSchema } from './schemas/user.schema';
-
-
