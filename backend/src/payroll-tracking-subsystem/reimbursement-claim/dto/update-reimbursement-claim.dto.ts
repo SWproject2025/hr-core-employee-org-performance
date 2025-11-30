d73e24a9d65fbee821de1cd52ef62d1013561bd3
salma gouda <salma.gouda@student.giu-uni.de>
2025-11-30 13:49:23 +0200

    cleaned branch

--- backend/src/payroll-tracking-subsystem/reimbursement-claim/dto/update-reimbursement-claim.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReimbursementClaimDto } from './create-reimbursement-claim.dto';
-
-export class UpdateReimbursementClaimDto extends PartialType(CreateReimbursementClaimDto) {}
