e231a5004321edec65935d1410595d511e33f088
slinder01 <tachack01@gmail.com>
2025-12-01 20:55:44 +0200

    payroll config ml2

--- backend/src/payroll-configuration/payroll-configuration.module.ts
+++ backend/src/payroll-configuration/payroll-configuration.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PayrollConfigurationController } from './payroll-configuration.controller';
+
 import { PayrollConfigurationService } from './payroll-configuration.service';
 import { CompanyWideSettings, CompanyWideSettingsSchema } from './models/CompanyWideSettings.schema';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -11,6 +11,19 @@ import { signingBonus, signingBonusSchema } from './models/signingBonus.schema';
 import { taxRules, taxRulesSchema } from './models/taxRules.schema';
 import { terminationAndResignationBenefits, terminationAndResignationBenefitsSchema } from './models/terminationAndResignationBenefits';
 import { payGrade } from './models/payGrades.schema';
+//gaurd
+import { RolesGuard } from '../Common/Gaurds/roles.gaurd';
+//controllers
+import { PayrollPolicyController } from './Controllers/payroll-policy/payroll-policy.controller';
+import { AllowanceController } from './Controllers/allowance/allowance.controller';
+import { PayGradeController } from './Controllers/pay-grade/pay-grade.controller';
+import {InsuranceBracketController} from "./Controllers/insurance-bracket/insurance-bracket.controller";
+import { LegalRulesController } from './Controllers/legal-rules/legal-rules.controller';
+import { CompanyWideSettingsController } from './Controllers/company-wide-settings/company-wide-settings.controller';
+import { PayTypeController } from './Controllers/pay-type/pay-type.controller';
+import { SigningBonusController } from './Controllers/signing-bonus/signing-bonus.controller';
+import { TerminationResignationBenefitsController } from './Controllers/termination-resignation-benefits/termination-resignation-benefits.controller';
+
 
 @Module({
   imports: [
@@ -26,8 +39,11 @@ import { payGrade } from './models/payGrades.schema';
       { name: payGrade.name, schema: payTypeSchema }
     ]),
   ],
-  controllers: [PayrollConfigurationController],
-  providers: [PayrollConfigurationService],
+  controllers: [PayrollPolicyController, AllowanceController, PayGradeController, InsuranceBracketController, CompanyWideSettingsController,
+    LegalRulesController, PayTypeController, SigningBonusController, TerminationResignationBenefitsController,
+
+  ],
+  providers: [PayrollConfigurationService, RolesGuard],
   exports:[PayrollConfigurationService]
 })
 export class PayrollConfigurationModule { }
